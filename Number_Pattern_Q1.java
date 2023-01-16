09fe3889231aa761077a7e5d32a20b177a2a688c
kishan1721 <72886959+kishan1721@users.noreply.github.com>
2023-01-16 10:45:02 +0530

    Add files via upload

---
+++ Number_Pattern_Q1.java
@@ -0,0 +1,22 @@
+public class Number_Pattern
+{
+    public static void printNums(int n) 
+    { 
+        int i, j,num; 
+        for(i=0; i<n; i++)
+        { 
+            num=1; 
+            for(j=0; j<=i; j++) // inner loop for rows
+            {  
+                System.out.print(num+ " ");  
+                num++; 
+            }  
+            System.out.println(); 
+        } 
+    } 
+       public static void main(String args[]) 
+    { 
+        int n = 5; 
+        printNums(n); 
+    } 
+}

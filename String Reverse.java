c37a2033b83b2e57044fae85a7809c0b649c45db
kishan1721 <72886959+kishan1721@users.noreply.github.com>
2023-01-17 10:43:47 +0530

    Add files via upload

---
+++ String Reverse.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+  
+class Reversestring {
+    public static void main (String[] args) {
+		String originalStr = "Hello";
+		String reversedStr = "";
+		for (int i = 0; i < originalStr.length(); i++) {
+  			reversedStr = originalStr.charAt(i) + reversedStr;
+		}
+		System.out.println("Reversed string: "+ reversedStr);
+}}

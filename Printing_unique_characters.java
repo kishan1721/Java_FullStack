71c3d766c3938469c1224491e02dc1fc16e1b91e
kishan1721 <72886959+kishan1721@users.noreply.github.com>
2023-01-20 10:10:09 +0530

    Add files via upload

---
+++ Printing_unique_characters.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ 
+class Unique_String{
+ 
+public static void main(String[] args)
+{
+    String str = "Kishan vyas";
+ 
+    for (int i = 0; i < str.length(); i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < str.length(); j++)
+        {
+            if (str.charAt(i) == str.charAt(j) && i != j)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            System.out.print(str.charAt(i));
+    }
+}
+}
